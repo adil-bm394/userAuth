07105aabec70838aab7892bd562d25d81eb3de98
adil-bm394 <adil@binmile.com>
2024-08-24 14:10:16 +0530

    update database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
+const serverConfig = require("./server-config");
 
-const sequelize = new Sequelize("fakeDatabase", "adil", "adil@123", {
-  host: "localhost",
+const sequelize = new Sequelize( serverConfig.DATABASE_NAME, serverConfig.USER, serverConfig.PASSWORD, {
+  host: serverConfig.HOST,
   dialect: "mysql",
 });
 
--- config/server-config.js
+++ config/server-config.js
@@ -6,5 +6,9 @@ module.exports = {
   PORT: process.env.PORT,
   mail: process.env.mail,
   pass: process.env.pass,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_NAME:process.env.DATABASE_NAME,
+  USER:process.env.USER,
+  PASSWORD:process.env.PASSWORD,
+  HOST:process.env.HOST
 };
